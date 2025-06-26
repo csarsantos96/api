a65715d31af4f1a29dcbe9822c8fcff252941fc6
Cesar Santos <csar.santos18@gmail.com>
2025-06-26 17:29:13 -0500

    remove pasta resources do controle de versão

--- src/main/resources/db/migration/V2__inserir_usuario_admin.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO usuarios (nome, email, senha)
-VALUES ('Administrador', 'admin@forumhub.com', '$2a$10$bbbmMmkd.WdxrXULoFW0gotWE1poTrb1qLpZGo0MoRdQtPfP2Cq');
--- src/main/resources/db/migration/V3__insert_admin_if_not_exists.sql
+++
@@ -1,7 +0,0 @@
--- V3__insert_admin_if_not_exists.sql
-
-INSERT INTO usuarios (nome, email, senha)
-SELECT 'Admin', 'admin@forumhub.com', '$2a$10$bbbmMmkd.WdxrXULoFW0gotWE1poTrb1qLpZGo0MoRdQtPfP2Cq'
-    WHERE NOT EXISTS (
-    SELECT 1 FROM usuarios WHERE email = 'admin@forumhub.com'
-);
